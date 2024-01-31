79595be5a7874f9e191a5e634b2b3212afabc40e
Henry Lin <henry.ys.lin@gmail.com>
2024-01-31 11:29:32 +0800

    Change CopycatLayerModel to use ISimpleCopycatModel and fix the look

--- src/main/java/com/copycatsplus/copycats/content/copycat/layer/CopycatLayerModel.java
+++ src/main/java/com/copycatsplus/copycats/content/copycat/layer/CopycatLayerModel.java
@@ -1,48 +1,67 @@
 package com.copycatsplus.copycats.content.copycat.layer;
 
+import com.copycatsplus.copycats.content.copycat.ISimpleCopycatModel;
 import com.simibubi.create.content.decoration.copycat.CopycatModel;
-import com.simibubi.create.foundation.model.BakedModelHelper;
-import com.simibubi.create.foundation.model.BakedQuadHelper;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.block.model.BakedQuad;
 import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.core.Direction;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.phys.AABB;
-import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.client.model.data.ModelData;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class CopycatLayerModel extends CopycatModel {
+import static com.copycatsplus.copycats.content.copycat.ISimpleCopycatModel.MutableCullFace.*;
 
-    private static final double[] SIZE_BY_LAYER = new double[]{0.0, 0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0};
+public class CopycatLayerModel extends CopycatModel implements ISimpleCopycatModel {
 
     public CopycatLayerModel(BakedModel originalModel) {
         super(originalModel);
     }
 
     @Override
-    protected List<BakedQuad> getCroppedQuads(BlockState state, Direction side, RandomSource rand, BlockState material, ModelData wrappedData, RenderType renderType) {
-        BakedModel originalModel = getModelOf(material);
-        if (originalModel instanceof CopycatLayerModel impl)
-            return impl.originalModel.getQuads(state, side, rand, wrappedData, renderType);
+    protected List<BakedQuad> getCroppedQuads(BlockState state, Direction side, RandomSource rand, BlockState material,
+                                              ModelData wrappedData, RenderType renderType) {
         BakedModel model = getModelOf(material);
         List<BakedQuad> templateQuads = model.getQuads(material, side, rand, wrappedData, renderType);
         List<BakedQuad> quads = new ArrayList<>();
+
+        int layer = state.getValue(CopycatLayerBlock.LAYERS);
         Direction facing = state.getValue(CopycatLayerBlock.FACING);
-        for (BakedQuad quad : templateQuads) {
-            switch (facing) {
-                case UP -> quads.add(BakedQuadHelper.cloneWithCustomGeometry(quad, BakedModelHelper.cropAndMove(quad.getVertices(), quad.getSprite(), AABB.ofSize(Vec3.ZERO, 2, SIZE_BY_LAYER[state.getValue(CopycatLayerBlock.LAYERS)], 2), Vec3.ZERO)));
-                case DOWN -> quads.add(BakedQuadHelper.cloneWithCustomGeometry(quad, BakedModelHelper.cropAndMove(quad.getVertices(), quad.getSprite(), AABB.ofSize(Vec3.ZERO.add(0, 1, 0), 2, SIZE_BY_LAYER[state.getValue(CopycatLayerBlock.LAYERS)], 2), Vec3.ZERO)));
-                case WEST -> quads.add(BakedQuadHelper.cloneWithCustomGeometry(quad, BakedModelHelper.cropAndMove(quad.getVertices(), quad.getSprite(), AABB.ofSize(Vec3.ZERO.add(1, 0, 1), SIZE_BY_LAYER[state.getValue(CopycatLayerBlock.LAYERS)], 2, 2), Vec3.ZERO)));
-                case EAST -> quads.add(BakedQuadHelper.cloneWithCustomGeometry(quad, BakedModelHelper.cropAndMove(quad.getVertices(), quad.getSprite(), AABB.ofSize(Vec3.ZERO, SIZE_BY_LAYER[state.getValue(CopycatLayerBlock.LAYERS)], 2, 2), Vec3.ZERO)));
-                case SOUTH -> quads.add(BakedQuadHelper.cloneWithCustomGeometry(quad, BakedModelHelper.cropAndMove(quad.getVertices(), quad.getSprite(), AABB.ofSize(Vec3.ZERO, 2, 2, SIZE_BY_LAYER[state.getValue(CopycatLayerBlock.LAYERS)]), Vec3.ZERO)));
-                case NORTH -> quads.add(BakedQuadHelper.cloneWithCustomGeometry(quad, BakedModelHelper.cropAndMove(quad.getVertices(), quad.getSprite(), AABB.ofSize(Vec3.ZERO.add(1, 0, 1), 2, 2, SIZE_BY_LAYER[state.getValue(CopycatLayerBlock.LAYERS)]), Vec3.ZERO)));
-            }
+
+        if (facing.getAxis().isVertical()) {
+            boolean flipY = facing == Direction.DOWN;
+            assemblePiece(
+                    templateQuads, quads, 0, flipY,
+                    vec3(0, 0, 0),
+                    aabb(16, layer, 16),
+                    cull(UP)
+            );
+            assemblePiece(
+                    templateQuads, quads, 0, flipY,
+                    vec3(0, layer, 0),
+                    aabb(16, layer, 16).move(0, 16 - layer, 0),
+                    cull(DOWN)
+            );
+        } else {
+            int rot = (int) facing.toYRot();
+            assemblePiece(
+                    templateQuads, quads, rot, false,
+                    vec3(0, 0, 0),
+                    aabb(16, 16, layer),
+                    cull(SOUTH)
+            );
+            assemblePiece(
+                    templateQuads, quads, rot, false,
+                    vec3(0, 0, layer),
+                    aabb(16, 16, layer).move(0, 0, 16 - layer),
+                    cull(NORTH)
+            );
         }
+
+
         return quads;
     }
 }
