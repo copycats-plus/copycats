72806c4a9410b1b5dbf40db3e7b627700db15bac
Henry Lin <henry.ys.lin@gmail.com>
2024-02-05 23:23:09 +0800

    Add new cat variant - Copy Cat
    - Only spawns in full moon
    - Placeholder texture

--- src/main/java/com/copycatsplus/copycats/datagen/CCTagGen.java
+++ src/main/java/com/copycatsplus/copycats/datagen/CCTagGen.java
@@ -1,18 +1,25 @@
 package com.copycatsplus.copycats.datagen;
 
 import com.copycatsplus.copycats.CCBlocks;
+import com.copycatsplus.copycats.CCCatVariants;
+import com.copycatsplus.copycats.CCDataProviderTypes;
 import com.copycatsplus.copycats.Copycats;
 import com.copycatsplus.copycats.compat.Mods;
 import com.simibubi.create.foundation.data.TagGen;
 import com.tterrag.registrate.providers.ProviderType;
 import com.tterrag.registrate.providers.RegistrateTagsProvider;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.tags.CatVariantTags;
 import net.minecraft.tags.TagKey;
+import net.minecraft.world.entity.animal.CatVariant;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class CCTagGen {
+
     public static void addGenerators() {
         Copycats.getRegistrate().addDataGenerator(ProviderType.BLOCK_TAGS, CCTagGen::genBlockTags);
+        Copycats.getRegistrate().addDataGenerator(CCDataProviderTypes.CAT_VARIANT_TAGS, CCTagGen::genCatVariantTags);
     }
 
     private static void genBlockTags(RegistrateTagsProvider<Block> provIn) {
@@ -20,5 +27,11 @@ public class CCTagGen {
         prov.tag(TagKey.create(ForgeRegistries.Keys.BLOCKS, Mods.DIAGONAL_FENCES.rl("non_diagonal_fences")))
                 .add(CCBlocks.COPYCAT_FENCE.get())
                 .add(CCBlocks.WRAPPED_COPYCAT_FENCE.get());
+    }
+
+    private static void genCatVariantTags(RegistrateTagsProvider<CatVariant> provIn) {
+        TagGen.CreateTagsProvider<CatVariant> prov = new TagGen.CreateTagsProvider<>(provIn, variant -> BuiltInRegistries.CAT_VARIANT.getResourceKey(variant).flatMap(BuiltInRegistries.CAT_VARIANT::getHolder).orElse(null));
+        prov.tag(CatVariantTags.FULL_MOON_SPAWNS)
+                .add(CCCatVariants.COPY_CAT.key());
     }
 }
