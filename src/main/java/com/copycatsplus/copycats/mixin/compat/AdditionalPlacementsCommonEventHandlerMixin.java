8c0cb8a3c598817f76a8ce61146a0dacbd902eca
Bennyboy1695 <benny.boy1695@gmail.com>
2024-02-10 15:38:10 +0000

    Change additionalplacements mixin to be fabric and forge compatible with 0 changes

--- src/main/java/com/copycatsplus/copycats/mixin/compat/AdditionalPlacementsCommonEventHandlerMixin.java
+++ src/main/java/com/copycatsplus/copycats/mixin/compat/AdditionalPlacementsCommonEventHandlerMixin.java
@@ -1,18 +1,22 @@
 package com.copycatsplus.copycats.mixin.compat;
 
-
 import com.copycatsplus.copycats.Copycats;
 import com.firemerald.additionalplacements.common.CommonModEventHandler;
+import com.firemerald.additionalplacements.common.CommonModEvents;
 import com.firemerald.additionalplacements.common.ConfigCommon;
 import net.minecraft.resources.ResourceLocation;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(value = CommonModEventHandler.class, priority = 1100)
+@Pseudo
+@Mixin(value = {CommonModEventHandler.class, CommonModEvents.class}, priority = 1100)
 public class AdditionalPlacementsCommonEventHandlerMixin {
 
-    @Redirect(method = "tryAdd", at = @At(value = "INVOKE", target = "Lcom/firemerald/additionalplacements/common/ConfigCommon;isValidForGeneration(Lnet/minecraft/resources/ResourceLocation;)Z"), remap = false, require = 0, expect = 0)
+    //Will appear to error but works and builds fine.
+    //Forces our mod to never get extra states
+    @Redirect(method = "tryAdd", at = @At(value = "INVOKE", target = "Lcom/firemerald/additionalplacements/common/ConfigCommon;isValidForGeneration(L;)Z"), remap = false, require = 0, expect = 0)
     private static boolean copycats$disableModCompat(ConfigCommon instance, ResourceLocation blockName) {
         if (blockName.getNamespace().equalsIgnoreCase(Copycats.MODID)) {
             return false;
