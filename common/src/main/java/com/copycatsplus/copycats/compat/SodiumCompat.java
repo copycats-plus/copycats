135a98f1a5e63adba7f6ab9a977f36b7198e66e5
Henry Lin <henry.ys.lin@gmail.com>
2024-09-15 12:06:37 +0800

    Fix sodium compat for 1.19

--- common/src/main/java/com/copycatsplus/copycats/compat/SodiumCompat.java
+++ common/src/main/java/com/copycatsplus/copycats/compat/SodiumCompat.java
@@ -1,12 +1,13 @@
 package com.copycatsplus.copycats.compat;
 
+import com.copycatsplus.copycats.mixin.compat.rubidium.WorldSliceAccessor;
 import me.jellysquid.mods.sodium.client.world.WorldSlice;
 import net.minecraft.world.level.BlockGetter;
 
 public class SodiumCompat {
     public static BlockGetter unwrapSodiumLevel(BlockGetter level) {
         if (level instanceof WorldSlice slice) {
-            return slice.world;
+            return ((WorldSliceAccessor) slice).getWorld();
         }
         return level;
     }
