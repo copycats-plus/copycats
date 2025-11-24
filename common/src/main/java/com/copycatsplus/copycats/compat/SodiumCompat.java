85f6a5140e9b0af7b17f9da9fe60697524d7051a
byquanton <32410361+byquanton@users.noreply.github.com>
2025-11-24 18:36:39 +0100

    Fix Sodium 0.5.12+ for Copycat 1.20.1 Fabric

--- common/src/main/java/com/copycatsplus/copycats/compat/SodiumCompat.java
+++ common/src/main/java/com/copycatsplus/copycats/compat/SodiumCompat.java
@@ -1,13 +1,27 @@
 package com.copycatsplus.copycats.compat;
 
 import me.jellysquid.mods.sodium.client.world.WorldSlice;
+import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.world.level.BlockGetter;
 
+import java.lang.reflect.Field;
+
 public class SodiumCompat {
+
     public static BlockGetter unwrapSodiumLevel(BlockGetter level) {
         if (level instanceof WorldSlice slice) {
-            return slice.world;
+            return getPrivateField(slice);
         }
         return level;
+    }
+
+    private static ClientLevel getPrivateField(WorldSlice instance) {
+        try {
+            Field f = WorldSlice.class.getDeclaredField("world");
+            f.setAccessible(true);
+            return (ClientLevel) f.get(instance);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to access field", e);
+        }
     }
 }
